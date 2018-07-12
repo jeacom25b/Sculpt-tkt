23bf8dbb5d8ef538249ddf2bfcbd60468b00691c
Jean Da Costa Machado <blendeto@gmail.com>
2018-07-12 14:42:44 -0300

    Plot new feature: Field Remeshing.. again

--- field_remesh.py
+++ field_remesh.py
@@ -1,5 +1,6 @@
 import bpy
 import bmesh
+import random
 
 
 class Field:
@@ -11,41 +12,38 @@ class Field:
         self.build_field()
 
     def build_field(self):
+        ## Get the curvature field for following Bmesh
         for vert in self.bm.verts:
-            vector1 = sorted([face.normal for face in vert.link_faces],
-                             key=lambda vector: vector.dot(vert.normal))[-1].cross(vert.normal)
-            vector2 = vector1.cross(vert.normal)
-            print(vector1)
-            self.field[vert] = (vector1, vector2)
+            normals = sorted([edge.other_vert(vert).normal for edge in vert.link_edges],
+                             key=lambda vector: vector.dot(vert.normal))
+            self.field[vert.index] = normals[0].cross(vert.normal)
 
-    def stroke_seams(self):
-        # Todo implement splitting
-        pass
+    def stroke_seams(self, seed_distance=0.1, seed_num=10):
 
-    def preview(self, outmesh):
+    def preview(self, out):
+        ## Temporary preview method for testing
+        self.bm.verts.ensure_lookup_table()
         bm = bmesh.new()
-        for vert in self.field.keys():
-            vectors = self.field[vert]
-            v1 = bm.verts.new(vert.co)
-            v2 = bm.verts.new(((vectors[0] * 0.5) + vert.co))
-            v3 = bm.verts.new(((vectors[1] * 0.5) + vert.co))
-
+        for i, vec in self.field.items():
+            v1 = bm.verts.new(self.bm.verts[i].co)
+            v2 = bm.verts.new(v1.co + vec)
             bm.edges.new((v1, v2))
-            bm.edges.new((v1, v3))
-        bm.to_mesh(outmesh)
 
-class Field_remesh(bpy.types.Operator):
-    bl_idname = "sculptkt.field_remesh"
-    bl_label = "Field Remesh"
-    bl_description = ""
-    bl_options = {"REGISTER", "UNDO"}
+        bm.to_mesh(out.data)
 
-    @classmethod
-    def poll(cls, context):
-        return context.active_object.type == "MESH"
+    class Field_remesh(bpy.types.Operator):
+        bl_idname = "sculptkt.field_remesh"
+        bl_label = "Field Remesh"
+        bl_description = ""
+        bl_options = {"REGISTER", "UNDO"}
 
-    def execute(self, context):
-        mesh = context.active_object.data
-        my_field = Field(mesh)
-        my_field.preview(context.scene.objects["out"].data)
-        return {"FINISHED"}
+        @classmethod
+        def poll(cls, context):
+            return context.active_object.type == "MESH"
+
+        def execute(self, context):
+            mesh = context.active_object
+            out = context.scene.objects["out"]
+            my_field = Field(mesh.data)
+            my_field.preview(out)
+            return {"FINISHED"}
